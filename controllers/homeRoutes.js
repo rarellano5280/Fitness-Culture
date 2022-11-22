72333e6ec13817337f05f46178d62fbeaa0e3cae
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-11-21 18:23:15 -0700

    made login page the default page on page open

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,17 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/homepage', async (req, res) => {
   try {
     res.render('homepage', {
       logged_in: req.session.logged_in,
@@ -29,16 +39,6 @@ router.get('/profile', withAuth, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
 });
 
 module.exports = router;
