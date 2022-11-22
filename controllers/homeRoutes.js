d59ae41dce52cf4157a5746f5aea8bd395dc302b
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-11-21 20:28:03 -0700

    draft login screen

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,4 +41,21 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  
+
+  res.render('signup');
+});
+
+
 module.exports = router;
