e32d283c96ec195e4e6363a6c49345df969f147e
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-11-28 18:59:00 -0700

    added accounts page and linked to users using handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,6 +41,23 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+router.get('/account', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('account', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
