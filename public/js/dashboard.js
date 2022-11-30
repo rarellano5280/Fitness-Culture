11766301b2a516683af94164d99ee07ef84a069d
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-11-29 20:44:14 -0700

    added event listeners for dashboard tiles

---
+++ public/js/dashboard.js
@@ -0,0 +1,23 @@
+document.getElementById('nutritionBtn').addEventListener('click', () => {
+    window.location.href = '/nutrition'
+})
+
+document.getElementById('programBtn').addEventListener('click', () => {
+    window.location.href = '/program'
+});
+
+document.getElementById('measurementsBtn').addEventListener('click', () => {
+    window.location.href = '/measurements'
+});
+
+document.getElementById('mealPrepBtn').addEventListener('click', () => {
+    window.location.href = '/meal-prep'
+});
+
+document.getElementById('maxBtn').addEventListener('click', () => {
+    window.location.href = '/max'
+});
+
+document.getElementById('accountBtn').addEventListener('click', () => {
+    window.location.href = '/account'
+});
